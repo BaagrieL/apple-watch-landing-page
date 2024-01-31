df461c644705f99aecc832fea47c64ee34ef171d
Gabriel <raulgabruel12@gmail.com>
2024-01-31 18:17:11 -0300

    Criei o html e css e importei as imagens do projeto

---
+++ script.js
@@ -0,0 +1,28 @@
+const verdeCipreste = {
+    nome: 'Verder-cipreste',
+    pasta: 'imagens-verde-cipreste'
+};
+
+const azulInverno = {
+    nome: 'Azul-inverno',
+    pasta: 'imagens-azul-inverno'
+};
+
+const meiaNoite = {
+    nome: 'Meia-noite',
+    pasta: 'imagens-meia-noite'
+};
+
+const estelar = {
+    nome: 'Estelar',
+    pasta: 'imagens-estelar'
+};
+
+const rosaClaro = {
+    nome: 'Rosa-claro',
+    pasta: 'imagens-rosa-claro'
+};
+
+const cores = [verdeCipreste, azulInverno ,meiaNoite ,estelar ,rosaClaro];
+
+console.log(cores[2]);

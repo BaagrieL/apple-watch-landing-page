8f8021e22a0d65afe6c9e1c9de6b6c34fde64952
Gabriel <raulgabruel12@gmail.com>
2024-01-31 19:43:53 -0300

    Adicionei a funcionalidade de mudar a posição da imagem do relógio e mudar o tamanho do relógio em si

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const imagemVisualizacao = document.getElementById('imagem-visualizacao');
+const opcaoImagemSelecionada = document.querySelectorAll('[name="opcao-imagem"]');
+const opcaoTamanhoSelecionada = document.querySelectorAll('[name="opcao-tamanho"]');
+const tituloProduto = document.getElementById('titulo-produto');
+
 const verdeCipreste = {
     nome: 'Verder-cipreste',
     pasta: 'imagens-verde-cipreste'
@@ -23,6 +28,32 @@ const rosaClaro = {
     pasta: 'imagens-rosa-claro'
 };
 
-const cores = [verdeCipreste, azulInverno ,meiaNoite ,estelar ,rosaClaro];
+const opcoesCores = [verdeCipreste, azulInverno ,meiaNoite ,estelar ,rosaClaro];
+const opcoesTamanho = ['41 mm', '45 mm'];
 
-console.log(cores[2]);
+let imagemSelecionada = 1;
+let tamanhoSelecionado = 1;
+
+
+
+function trocarImagem () {
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
+
+    imagemSelecionada = idOpcaoSelecionada.charAt(0);
+    imagemVisualizacao.src = `./imagens/opcoes-cores/imagens-azul-inverno/imagem-${imagemSelecionada}.jpeg`;
+};
+
+opcaoImagemSelecionada.forEach(element => {
+    element.addEventListener("click", trocarImagem);
+});
+
+function trocartamanho () {
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
+    tamanhoSelecionado = idOpcaoSelecionada.charAt(0);
+    
+    tituloProduto.innerText = `Pulseira loop esportiva azul-inverno para caixa de ${opcoesTamanho[tamanhoSelecionado]}`
+}
+
+opcaoTamanhoSelecionada.forEach(element => {
+    element.addEventListener("click", trocartamanho);
+});
